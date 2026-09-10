f599b694f7915b2dd8c23d574370115fcfed8bb9
Thomas-Alves <tj2alves@gmail.com>
2026-09-10 13:51:03 -0400

    have to complete the commented work at home
    -school computer doesnt allow access
    - have to work at home

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -12,20 +12,27 @@ const unsigned int TARGET_FPS = 50;
 float time = 0;
 int main()
 {
-    InitWindow(1200, 800," Thomas Alves 101422210 - if you can see this then good foryou becasue i can't");
+    InitWindow(1200, 800," lab 1- game2005");
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(WHITE);
-            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
+            DrawText("Thomas Alves ID:101422210", 10, 10, 20, LIGHTGRAY);
 
 
             time += 1;
 
             GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+            //set up a iretive game loop with a repeting update function, also render time on the top right of the window.
+            //move and object which starts at a given postion and moves each fram acorrding to the equations from the lap overview. 
 
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+            EndDrawing();
 
         EndDrawing();
     }
